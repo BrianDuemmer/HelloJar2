32398ce38a3dcf46c267f73c7a55eb831aa45725
BrianDuemmer <bduemmer1@gmail.com>
2016-12-02 14:36:03 -0500

    Duemmer_Commit_12-2-16
    Added MicroLog support

--- src/pack/MainClass.java
+++ src/pack/MainClass.java
@@ -1,14 +1,49 @@
 package pack;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+
+import net.sf.microlog.core.Level;
+import net.sf.microlog.core.Logger;
+import net.sf.microlog.core.LoggerFactory;
+import net.sf.microlog.core.appender.ConsoleAppender;
+import net.sf.microlog.core.format.PatternFormatter;
+import net.sf.microlog.midp.file.FileAppender;
+import net.sf.microlog.core.Appender;
 
 
-public class MainClass {
+
+public class MainClass 
+{
+	// Format string for all loggers
+	private static String fmtString = "%d{HH:mm:ss:SSS} [%P] [%c{1}] %m";
 	
-	public static final Logger log = LogManager.getLogger("myLogger");
+	// Set the logging directory to a subdirectory of the home directory 
+	private static String logDir = System.getProperty("user.home") + "/logging";
 	
-	public static void main(String[] args) {
+	// Appender stuff
+	private static PatternFormatter formPattern;
+	private static ConsoleAppender consoleAppender;
+	private static FileAppender fileAppender;
+	
+	
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		// make sure the logging dir exists
+		mkdirIfNeeded(logDir);
+		
+		// initialize the appenders
+		initAppenders();
+		
+		// Initialize the logger
+		Logger log = LoggerFactory.getLogger(MainClass.class);
+		configLogger(log);
+		
 		for(int i = 0; i < 5; i++)
 		{
 			log.info("Helloooooooooooooooo World!");
@@ -20,5 +55,91 @@ public class MainClass {
 		}
 
 	}
+	
+	
+	
+	public static Logger configLogger(Logger log)
+	{
+		// set the root level
+		log.setLevel(Level.TRACE);
+		
+		// Add the appenders
+		log.addAppender(consoleAppender);
+		
+		// return the finished logger
+		return log;
+	}
+	
+	
+	
+	
+	public static void initAppenders()
+	{
+		// Setup the pattern formatter to the proper pattern
+		formPattern = new PatternFormatter();
+		formPattern.setPattern(fmtString);
+		
+		// Setup console appender and add it to the logger
+		consoleAppender = new ConsoleAppender();
+		consoleAppender.setFormatter(formPattern);
+		
+		
+		
+		// Get the subdirectory and filename that we will log to
+		
+		// initialize the calendar to the date
+		Date date = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		
+		// get the date components
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		
+		// format the subdirectory name
+		String dirName = year +"-"+ month +"-" + day;
+		
+		// get the time components
+		int hour = cal.get(Calendar.HOUR_OF_DAY);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+		// format the log file name
+		String logName = hour +"h-"+ minute +"m-"+ second + "s_roboLog";
+		
+		
+		// make sure the logging subdirectory exists
+		mkdirIfNeeded(logDir +"/"+ dirName);
+		
+		// full path to the log file
+		String fullLogPath = logDir +"/"+ dirName +"/"+ logName +".log";
+		
+		// Initialize the FileAppender
+		fileAppender = new FileAppender();
+		fileAppender.setFormatter(formPattern);
+		fileAppender.setFileName(fullLogPath);
+	}
+	
+	
+	
+	private static void mkdirIfNeeded(String directoryName)
+	{
+		File theDir = new File(directoryName); 
+		if (!theDir.exists())
+		    theDir.mkdirs();
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/pack/MyfileAppender.java
@@ -0,0 +1,87 @@
+package pack;
+
+import java.io.IOException;
+
+import net.sf.microlog.core.Appender;
+import net.sf.microlog.core.Formatter;
+import net.sf.microlog.core.Level;
+
+
+
+
+
+public class MyfileAppender implements Appender 
+{
+
+	@Override
+	public void clear() 
+	{
+
+
+		
+	}
+
+	@Override
+	public void close() throws IOException 
+	{
+
+		
+	}
+
+	@Override
+	public void doLog(String arg0, String arg1, long arg2, Level arg3, Object arg4, Throwable arg5) 
+	{
+		
+		
+	}
+
+	@Override
+	public Formatter getFormatter() 
+	{
+		
+		return null;
+	}
+
+	@Override
+	public long getLogSize() 
+	{
+		
+		return 0;
+	}
+
+	@Override
+	public String[] getPropertyNames() 
+	{
+
+		return null;
+	}
+
+	@Override
+	public boolean isLogOpen() 
+	{
+
+		return false;
+	}
+
+	@Override
+	public void open() throws IOException 
+	{
+		
+		
+	}
+
+	@Override
+	public void setFormatter(Formatter arg0) 
+	{
+		
+		
+	}
+
+	@Override
+	public void setProperty(String arg0, String arg1) throws IllegalArgumentException 
+	{
+		
+		
+	}
+
+}
